6297c185fc046092fde4cca2a8f9705efc7a4fb9
Oleg Iurchenko <oiurchenko@mirantis.com>
2017-12-06 12:45:04 +0200

    .

--- src/com/mirantis1/mk/Salt.groovy
+++ src/com/mirantis1/mk/Salt.groovy
@@ -604,9 +604,9 @@ def checkResult(result, failOnError = true, printResults = true, printOnlyChange
 def waitForMinion(result) {
     def common = new com.mirantis.mk.Common()
     def matcher = result =~ /(?s).*salt_minion_service_restart.*?(changes:\[.*?\])/    
-    while(matcher.find()) {
+    if(matcher.find()) {
         common.infoMsg("!!!!")
-        if (matcher.groupCount() == 1 && matcher.group(1) != null && matcher.group(1).contains("pid")) {
+        if (matcher.group(1) != null && matcher.group(1).contains("pid")) {
             common.infoMsg("Salt minion service restart detected. Sleep 10 seconds to wait minion restart")
             sleep(10)
         }
