351ad20b0deec619cd92e8edd75194dd16ea61b4
Oleg Iurchenko <oiurchenko@mirantis.com>
2017-12-12 09:25:06 +0200

    .

--- src/com/mirantis1/mk/Salt.groovy
+++ src/com/mirantis1/mk/Salt.groovy
@@ -599,7 +599,6 @@ def checkResult(result, failOnError = true, printResults = true, printOnlyChange
 *
 * @param result    Parsed response of Salt API
 */
-@NonCPS
 def waitForMinion(result) {
     //println(result)
     println("T1")
