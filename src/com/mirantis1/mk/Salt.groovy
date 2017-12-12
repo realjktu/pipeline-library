06154f887fcf5628f1a56d1b302a3aef4806fc32
Oleg Iurchenko <oiurchenko@mirantis.com>
2017-12-12 10:26:33 +0200

    .

--- src/com/mirantis1/mk/Salt.groovy
+++ src/com/mirantis1/mk/Salt.groovy
@@ -607,8 +607,8 @@ def waitForMinion(result) {
     String resultStr=result.toString()
     common.infoMsg(resultStr)
     common.infoMsg("T1")
-    //Pattern regexRestart = Pattern.compile('.*salt_minion_service_restart.*?(changes:\\[.*?\\])', Pattern.DOTALL);
     Pattern regexRestart = Pattern.compile('.*salt_minion_service_restart.*?(changes:\\[.*?\\])', Pattern.DOTALL);
+    //Pattern regexRestart = Pattern.compile('.*salt_minion_service_restart.*?(changes:\\[.*?\\])', Pattern.DOTALL);
     common.infoMsg("T1.1")
     Matcher matcher = regexRestart.matcher(resultStr);
     //def matcher = (result =~ /(?m).*salt_minion_service_restart.*?(changes:\[.*?\])/ )
