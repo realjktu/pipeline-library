e1df8c8958d4e76513d545a68a8fad269f785f0f
Oleg Iurchenko <oiurchenko@mirantis.com>
2017-12-12 13:11:32 +0200

    .

--- src/com/mirantis1/mk/Salt.groovy
+++ src/com/mirantis1/mk/Salt.groovy
@@ -621,8 +621,8 @@ def waitForMinion(result) {
                                 resKey = k
                             }
                             resource = node[resKey]
-                            if(resKey.contains("salt_minion_service_restart") && resource instanceof Map && resource.keySet().contains("result")){                                
-                                if(resource["result"] || (resource["result"] instanceof String && resource["result"] == "true")){
+                            if(resKey.contains("salt_minion_service_restart") && resource instanceof Map && resource.keySet().contains("result")){
+                                if((resource["result"] instanceof Boolean && resource["result"]) || (resource["result"] instanceof String && resource["result"] == "true")){
                                     if(resource.changes.size() > 0){
                                         isMinionRestarted=true
                                     }
